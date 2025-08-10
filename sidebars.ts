ce92fb50bf367c4d42d85115ccef9a732e6fbfa2
Anton Kashirov <anton.kashirov@gmail.com>
2025-08-10 17:38:24 +0300

    feat: installation (#4)

--- sidebars.ts
+++ sidebars.ts
@@ -1,8 +1,20 @@
-import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
   docsSidebar: [
-    'intro',
+    "intro",
+    {
+      type: 'category',
+      label: 'Getting Started',
+      link: {
+        type: 'doc',
+        id: 'getting-started/index',
+      },
+      items: [
+        'getting-started/installation',
+        'getting-started/first-steps',
+      ],
+    },
   ],
 };
 
