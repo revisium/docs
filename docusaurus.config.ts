2af42ac43ee67b8fb885077b2ba6d50c2c96e66e
anton62k <anton.kashirov@gmail.com>
2025-08-10 17:35:14 +0300

    fix editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/revisium/docs/tree/main/",
+          editUrl: "https://github.com/revisium/docs/tree/master/",
         },
         blog: {
           showReadingTime: true,
